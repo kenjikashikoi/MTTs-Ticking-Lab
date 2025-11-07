756b0f3c23268f7ae656209b32c2258918378954
kenjikashikoi <bigj1409@gmail.com>
2025-11-07 20:28:02 +0100

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const cacheName = "tricking-lab-cache-v1";
+const assetsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assetsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
