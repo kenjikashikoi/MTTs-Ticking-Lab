0a3ee4ca41c857bf16dbdcf8e1a539f3fb6deadb
kenjikashikoi <bigj1409@gmail.com>
2025-11-09 11:03:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,48 @@
-const cacheName = "tricking-lab-cache-v1";
-const assetsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/askip.mp4",
-  "/bulgarian.mp4",
-  "/calf.mp4",
-  "/sissy.mp4",
-  "/stepups.mp4",
-  "/tib.mp4"
+const CACHE_NAME = 'tricking-lab-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/askip.mp4',
+  '/bulgarian.mp4',
+  '/stepups.mp4',
+  '/tib.mp4',
+  '/sissy.mp4',
+  '/calf.mp4',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(assetsToCache))
-      .then(() => self.skipWaiting())
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline resources');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
       return Promise.all(
-        keys.filter(key => key !== cacheName)
-            .map(key => caches.delete(key))
+        keyList.map((key) => {
+          if(key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
       );
     })
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
   );
 });
-
